1d7c7d32cbfdc7e057c6d50209d7785cf369f9d9
Fernanda Bollinger <feernanda.bollinger@gmail.com>
2025-05-14 16:44:22 -0300

    feat: adicionando interatividade

--- criar-cartas.c
+++ criar-cartas.c
@@ -88,5 +88,92 @@ int main()
     printf("Densidade populacional: %.2f \n", densidadePopulacional2);
     printf("PIB per Capita: %.2f \n", pibPercapta2);
 
+    int atributoEscolhido;
+
+    printf("Escolha uma opção para a comparação entre as cartas: \n");
+    printf("1. Comparar a populaçao.\n");
+    printf("2. Comparar a área. \n");
+    printf("3. Comparar o PIB. \n");
+    printf("4. Comparar o número de pontos turísticos. \n");
+    printf("5. Comparar a densidade demográfica. \n");
+    scanf("%d", &atributoEscolhido);
+
+    switch (atributoEscolhido)
+    {
+    case 1:
+        if (populacao1 > populacao2)
+        {
+            printf("Carta 1 é a vencedora com a população de %d, enquanto a carta 2 possui apenas %d. \n", populacao1, populacao2);
+        }
+        else if (populacao2 > populacao1)
+        {
+            printf("Carta 2 é a vencedora com a população de %d, enquanto a carta 2 possui apenas %d. \n", populacao2, populacao1);
+        }
+        else
+        {
+            printf("Empate! Número populacional igual. \n");
+        }
+        break;
+    case 2:
+        if (area1 > area2)
+        {
+            printf("Carta 1 é a vencedora, com a área de %.2f enquanto a carta 2 possui uma área de %.2f. \n", area1, area2);
+        }
+        else if (area2 > area1)
+        {
+            printf("Carta 2 é a vencedora, com a área de %.2f enquanto a carta 1 possui uma área de %.2f. \n", area2, area1);
+        }
+        else
+        {
+            printf("Empate! ambas possuem a mesma área. \n");
+        }
+        break;
+    case 3:
+        if (pib1 > pib2)
+        {
+            printf("Carta 1 é a vencedora, com o PIB de %.2f enquanto a carta 2 possui o PIB de %.2f. \n", pib1, pib2);
+        }
+        else if (pib2 > pib1)
+        {
+            printf("Carta 2 é a vencedora, com o PIB de %.2f enquanto a carta 1 possui um PIB de %.2f. \n", pib2, pib1);
+        }
+        else
+        {
+            printf("Empate! ambas possuem o mesmo PIB. \n");
+        }
+        break;
+    case 4:
+        if (numeroPontosTuristicos1 > numeroPontosTuristicos2)
+        {
+            printf("Carta 1 é a vencedora, com %d pontos turisticos enquanto a carta 2 possui %d. \n", numeroPontosTuristicos1, numeroPontosTuristicos2);
+        }
+        else if (numeroPontosTuristicos2 > numeroPontosTuristicos1)
+        {
+            printf("Carta 2 é a vencedora, com %d pontos turisticos enquanto a carta 1 possui %d. \n", numeroPontosTuristicos2, numeroPontosTuristicos1);
+        }
+        else
+        {
+            printf("Empate! ambas possuem a mesma quantidade de pontos turísticos.");
+        }
+        break;
+    case 5:
+        if (densidadePopulacional1 > densidadePopulacional2)
+        {
+            printf("Carta 2 é a vencedora ! Com a densidade demográfica de %.2f inferior a da carta 1 que é %.2f. \n", densidadePopulacional2, densidadePopulacional1);
+        }
+        else if (densidadePopulacional2 > densidadePopulacional1)
+        {
+            printf("Carta 1 é a vencedora ! Com a densidade demográfica de %.2f inferior a da carta 1 que é %.2f. \n", densidadePopulacional1, densidadePopulacional2);
+        }
+        else
+        {
+            printf("Empate! Ambas possuem a mesma densidade demografica \n");
+        }
+        break;
+    default:
+        printf("Valor invalido! Digite uma das opções. \n");
+        break;
+    }
+
     return 0;
 }
